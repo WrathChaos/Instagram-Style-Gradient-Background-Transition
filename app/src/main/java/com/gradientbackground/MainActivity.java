1a99db8b2001517fb44b7f33a545beceb8bf22a7
WrathChaos <wrathchaos@hotmail.com>
2017-01-22 20:23:40 +0300

    Added Items

---
+++ app/src/main/java/com/gradientbackground/MainActivity.java
@@ -0,0 +1,41 @@
+package com.gradientbackground;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout container;
+    AnimationDrawable anim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        container = (LinearLayout) findViewById(R.id.container);
+
+        anim = (AnimationDrawable) container.getBackground();
+        anim.setEnterFadeDuration(6000);
+        anim.setExitFadeDuration(2000);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (anim != null && !anim.isRunning())
+            anim.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (anim != null && anim.isRunning())
+            anim.stop();
+    }
+}
